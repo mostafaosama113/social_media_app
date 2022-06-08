15c163e38d515dd23c638fa2fc98f1843d359ad2
mostafa <hitmanx97@gmail.com>
2022-06-08 12:32:29 +0200

    post setting

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -12,6 +12,52 @@ import 'package:social_media_app/shared/manger/padding_manger.dart';
 import 'package:social_media_app/shared/manger/text_style_manger.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+Widget postSetting({
+  required IconData icon,
+  required String title,
+  required Function onClick,
+}) {
+  return Padding(
+    padding: const EdgeInsets.only(
+      top: 5,
+      right: 15,
+      left: 15,
+    ),
+    child: InkWell(
+      onTap: () => onClick(),
+      child: Material(
+        borderRadius: BorderRadius.circular(5),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 2,
+        child: Container(
+          height: 60.h,
+          width: double.infinity,
+          color: Colors.white,
+          alignment: Alignment.center,
+          child: Padding(
+            padding: defaultPadding,
+            child: Row(
+              children: [
+                Icon(
+                  icon,
+                  color: MyColor.lightBlue,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  title,
+                  style: defaultButtonStyle.copyWith(
+                    color: MyColor.lightBlue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
 Widget btn({
   required IconData icon,
   required String title,
@@ -140,7 +186,36 @@ class _PostWidgetState extends State<PostWidget> {
                     const Spacer(),
                     if (widget.postModel.userId == myUid)
                       InkWell(
-                        onTap: () {},
+                        onTap: () {
+                          showModalBottomSheet(
+                              backgroundColor: Colors.transparent,
+                              elevation: 0,
+                              barrierColor: Colors.transparent,
+                              context: context,
+                              builder: (context) {
+                                return SizedBox(
+                                  height: 140.h,
+                                  child: Column(
+                                    children: [
+                                      postSetting(
+                                        title: 'Edit post',
+                                        icon: Icons.edit,
+                                        onClick: () {
+                                          //todo : edit post
+                                        },
+                                      ),
+                                      postSetting(
+                                        title: 'Delete post',
+                                        icon: Icons.delete,
+                                        onClick: () {
+                                          //todo : delete post
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
                         highlightColor: Colors.transparent,
                         splashColor: Colors.transparent,
                         child: Icon(
